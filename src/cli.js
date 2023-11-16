4166c988cfb160e3c8bd24ac422e0f3d478a2afa
MikeDruzhin <mike.druzhin@gmail.com>
2023-11-16 23:29:58 +0300

    cli.js was changed

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default function greeting() {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+export const welcome = console.log('Welcome to the Brain Games!');
+export const name = readlineSync.question('May I have your name? ');
+export const hi = console.log(`Hello, ${name}!`);
+
+export function greeting() {
+  return `${welcome}\n${name}\n${hi}`;
 }
