82c9c23b3e6ac6866b8012b7a11da39f8aa68562
MikeDruzhin <mike.druzhin@gmail.com>
2023-11-16 16:31:26 +0300

    syntax was corrected

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,6 @@
 import readlineSync from 'readline-sync';
-const greeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-};
 
-export { greeting };
-
-
+export default function greeting() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
