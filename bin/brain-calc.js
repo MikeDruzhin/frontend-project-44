8a897f2f3b83bbc4a4df617333ecfdca013ce8f2
MikeDruzhin <mike.druzhin@gmail.com>
2023-11-19 20:51:13 +0300

    edited brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
-import { getRandomInt, intro, basicFunc2, congrats } from '../src/index.js';
+import {
+  getRandomInt, intro, basicFunc2, congrats,
+} from '../src/index.js';
 
 function arrayRandElement(arr) {
-    var rand = Math.floor(Math.random() * arr.length);
-    return arr[rand];
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
 }
 function doCalc(first, second, sign) {
-    let res = 0;
-    if (sign === '+') {
-        res = first + second;
-    } else if (sign === '-') {
-        res = first - second;
-    } else {
-        res = first * second;
-    }
-    return res;
+  let res = 0;
+  if (sign === '+') {
+    res = first + second;
+  } else if (sign === '-') {
+    res = first - second;
+  } else {
+    res = first * second;
+  }
+  return res;
 }
 const operations = ['+', '-', '*'];
 let res = 0;
@@ -23,16 +25,14 @@ const description = 'What is the result of the expression?';
 
 intro(description);
 for (let i = 0; i < 3; i += 1) {
-    const firstNum = getRandomInt();
-    const secondNum = getRandomInt();
-    const operator = arrayRandElement(operations);
-    const question = `${firstNum}${operator}${secondNum}`
-    res = doCalc(firstNum, secondNum, operator);
-    if (basicFunc2(question, res) !== res) {
-        break;
-    };
-    points += 1;
+  const firstNum = getRandomInt();
+  const secondNum = getRandomInt();
+  const operator = arrayRandElement(operations);
+  const question = `${firstNum}${operator}${secondNum}`;
+  res = doCalc(firstNum, secondNum, operator);
+  if (basicFunc2(question, res) !== res) {
+    break;
+  }
+  points += 1;
 }
 congrats(points);
-
-
